19e773d4daf9f5f65c6a605d6002f0cad93dc206
Andrew Lamy <lamyman11@gmail.com>
2021-11-02 19:46:13 -0400

    Chapter 10, Animating Robots

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -14,6 +14,7 @@ public class Robot : MonoBehaviour
     private Transform player;
     private float timeLastFired;
     private bool isDead;
+    public Animator robot;
 
     // Start is called before the first frame update
     void Start()
@@ -48,6 +49,6 @@ public class Robot : MonoBehaviour
 
     private void fire()
     {
-        Debug.Log("Fire");
+        robot.Play("Fire");
     }
 }
