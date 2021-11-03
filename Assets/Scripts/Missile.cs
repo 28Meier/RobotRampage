16b842ee3bc9e6e39583a402094f634cdf301cd6
Andrew Lamy <lamyman11@gmail.com>
2021-11-02 20:55:03 -0400

    Chapter 10, Adding Damage Effects

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Missile : MonoBehaviour
+public class Missle : MonoBehaviour
 {
     public float speed = 30f;
     public int damage = 10;
@@ -21,16 +21,6 @@ public class Missile : MonoBehaviour
     IEnumerator deathTimer()
     {
         yield return new WaitForSeconds(10);
-        Destroy(gameObject);
-    }
-
-    void OnCollisionEnter(Collision collider)
-    {
-        if (collider.gameObject.GetComponent<Player>() != null
-        && collider.gameObject.tag == "Player")
-        {
-            collider.gameObject.GetComponent<Player>().TakeDamage(damage);
-        }
         Destroy(gameObject);
     }
 }
